9695a64188331f6b0aee1ea6fb898e6c852c9f2a
nadiasiddiqah <nadiasiddiqah@gmail.com>
2020-10-29 23:14:12 -0700

    Initial Commit

---
+++ MyLocation_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocation_App
+//
+//  Created by Nadia Siddiqah on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
